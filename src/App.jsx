41f681e488ac0dbafbd80fdc7b28171dda0b9c63
thomasvergne <thomas.vergne.contact@gmail.com>
2023-04-12 14:16:46 +0200

    📱 Added website responsive.

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,12 @@ end
 `.trim();
 
 export default function App() {
-  return <header className="mx-auto h-screen grid place-items-center py-16">
+  return <header className="mx-auto h-screen grid place-items-center py-16 p-8">
     <div className="max-w-5xl mx-auto">
-      <h1 className="text-5xl font-title font-black w-3/4 text-white tracking-wide text-center mx-auto">
-        Uncover the true power of <span className="text-cyan-300 text">Goose language</span>
+      <h1 className="text-5xl font-title font-black lg:w-3/4 text-white tracking-wide text-center mx-auto">
+        Uncover the true power of <br/><span className="text-cyan-300 text">Goose language</span>
       </h1>
-      <p className="text-xl text-white mx-auto w-3/4 text-center mt-6 opacity-70 font-light">
+      <p className="text-xl text-white mx-auto lg:w-3/4 text-center mt-6 opacity-70 font-light">
         Discover a new way of programming with Goose. It's a simple, yet powerful language that will help you write code faster and more efficiently.
       </p>
       <div className="mt-12 bg-slate-900 p-4 mx-auto rounded-xl max-w-2xl shadow-2xl">
@@ -24,14 +24,14 @@ export default function App() {
           showLineNumbers customStyle={{ backgroundColor: 'rgb(15 23 42)' }}>
           {code}
         </SyntaxHighlighter>
-      </div>
-      <div className="flex max-w-2xl mx-auto w-full gap-4 mt-6">
-        <a href="" className="w-1/2 bg-slate-800 py-2 rounded-lg text-white font-medium text-lg font-title hover:shadow-lg hover:shadow-slate-900 transition-shadow duration-200 text-center">
-          Learn Goose
-        </a>
-        <a href="https://github.com/goose-language/goose" className="w-1/2 bg-cyan-500 py-2 rounded-lg text-white font-medium text-lg font-title hover:shadow-lg text-center hover:shadow-cyan-900 transition-shadow duration-200">
-          Star on GitHub
-        </a>
+        <div className="flex gap-4 mt-6">
+          <a href="" className="w-1/2 bg-slate-800 py-2 rounded-lg text-white font-medium text-lg font-title hover:shadow-lg hover:shadow-slate-900 transition-shadow duration-200 text-center">
+            Learn Goose
+          </a>
+          <a href="https://github.com/goose-language/goose" className="w-1/2 bg-cyan-500 py-2 rounded-lg text-white font-medium text-lg font-title hover:shadow-lg text-center hover:shadow-cyan-900 transition-shadow duration-200">
+            Star on GitHub
+          </a>
+        </div>
       </div>
     </div>
   </header>
